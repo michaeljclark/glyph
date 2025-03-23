16e0e9d7aa10c0132efb7b2d46cc8ae4b888ab61
Michael Clark <michaeljclark@mac.com>
2025-03-23 19:20:00 +1300

    make several minor changes to placeholder test cases
    - change break to nop and add break at end to jalib test
    - change break to nop in jtlib test
    - vary sign of immediate values in li test

--- tests/04_jalib_1.c
+++ tests/04_jalib_1.c
@@ -10,8 +10,9 @@ void t()
     };
     u16 i[] = {
         enc_jalib(7,0),
+        enc_nop(0),
+        enc_lib_i64(0,0),
         enc_break(0),
-        enc_lib_i64(0,0)
     };
     run_test("jalib_1", c, sizeof(c), i, sizeof(i));
 }
--- tests/07_li_1.c
+++ tests/07_li_1.c
@@ -8,13 +8,13 @@ void t()
     };
     u16 i[] = {
         enc_li_i64(0,0),
-        enc_li_i64(1,1),
+        enc_li_i64(1,-1),
         enc_li_i64(2,2),
-        enc_li_i64(3,3),
+        enc_li_i64(3,-3),
         enc_li_i64(4,4),
-        enc_li_i64(5,5),
+        enc_li_i64(5,-5),
         enc_li_i64(6,6),
-        enc_li_i64(7,7),
+        enc_li_i64(7,-7),
         enc_break(0)
     };
     run_test("li_1", c, sizeof(c), i, sizeof(i));
