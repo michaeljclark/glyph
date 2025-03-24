d2ce3a93962f1ba07c0da35a6ed8f1ba6f2590f7
Michael Clark <michaeljclark@mac.com>
2025-03-24 14:37:31 +1300

    remove mktemp dependency from differential test script

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
+tmp="glyph-temp"
+test -d ${tmp} || mkdir ${tmp}
 for test in $(ls tests/[0-9][0-9]_*.py | sort | sed 's#tests/\(.*\)\.py#\1#'); do
 	echo ${test}
-	A=$(mktemp test_${test}_XXXXXX)
-	B=$(mktemp test_${test}_XXXXXX)
-	./build/test_${test} > ${A}
-	./tests/${test}.py > ${B}
-	diff -u ${A} ${B}
-	rm -f ${A} ${B}
+	./build/test_${test} > ${tmp}/${test}_native
+	./tests/${test}.py > ${tmp}/${test}_python
+	diff -u ${tmp}/${test}_native ${tmp}/${test}_python
 done
+test -d ${tmp} && rm -fr ${tmp}
